9bfaa2db0a05984f0bfaa4c793c105c91432c4c6
Dominic Udousoro <dominicudousoro@gmail.com>
2025-11-28 19:14:18 +0100

    Fixes

--- app.py
+++ app.py
@@ -2624,5 +2624,3 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-    app.config.from_object(Config)
-    app.config['WTF_CSRF_ENABLED'] = False  
